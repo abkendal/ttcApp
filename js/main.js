71264de7e4b554a158a2c021522f9ef720fa1fa3
abkendal <kendal.adam@mail.com>
2015-08-05 19:12:56 -0400

    Add app.getStops function

--- js/main.js
+++ js/main.js
@@ -11,12 +11,24 @@ app.getGeo = function(){
 app.updatePosition = function(position) {
 	$geolocation[0] = position.coords.latitude;
 	$geolocation[1] = position.coords.longitude;
-
+	app.getStops($geolocation[0], $geolocation[1]);
 }
 
 app.geoError = function(){
 	 alert("No location info available. Error code: " + error.code);
 };
+
+app.getStops = function(lat, lon){
+	$.ajax({
+		url:'http://myttc.ca/near/'+ lat +','+ lon + '.json',
+		// url: 'http://myttc.ca/near/43.6483123,-79.39799909999999.json',
+		type: 'GET',
+		dataType: 'jsonp',
+		success: function(response){
+			console.log(response);
+		}
+	})
+}
 
 
 
