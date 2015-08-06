ba43a5471ecdbe057a3fc0adc1e9954c84a828fb
abkendal <kendal.adam@mail.com>
2015-08-06 14:58:01 -0400

    Make gulp scss work

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
-var jshint = require('gulp-jshint');
 
 gulp.task('styles', function(){
 	return gulp.src('css/*.scss')
@@ -11,17 +10,10 @@ gulp.task('styles', function(){
 });
 
 
-gulp.task('jshint', function() {
-	return gulp.src('js/*.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'));
-});
-
 
 
 gulp.task('watch', function() {
 	gulp.watch('css/*.scss', ['styles']);
-	gulp.watch('js/*.js', ['jshint']);
 });
 
 gulp.task('default', ['watch']);
